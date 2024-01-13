7ffd2b04b60ff8249f4293924e753092840c1268
will.fulton@fishtownanalytics.com <will.fulton@fishtownanalytics.com>
2024-01-13 23:59:42 +0000

    updated with sources

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,9 +1,11 @@
 with customers as (
     select * from {{ ref('stg_customers')}}
 ),
+
 orders as (
     select * from {{ ref('fct_orders')}}
 ),
+
 customer_orders as (
     select
         customer_id,
